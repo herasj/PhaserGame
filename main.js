0aa4d67bcf68062ff61b046f613fa8b4cf8fa28d
herasj <herasj@uninorte.edu.co>
2017-11-14 17:43:20 -0500

    Falta:
    Agregar Barriles
    Agregar jefe
    Agregar Peach
    Agregar BloquesD
    Modificar Mapa

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //Dimensiones del juego (Ancho,Alto)
- var juego = new Phaser.Game(768, 1000, Phaser.AUTO, '', {preload: preload, create: create, update: update, render: render});
+ var juego = new Phaser.Game(768, 1000, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 //Error corregido
                              
 // Preload, todo lo que se encuentre dentro de esta funcion se ejecutará mientras se cargan los componentes
@@ -10,20 +10,21 @@ juego.load.image('Block','Sprites/Blocks/Block1.gif');
 juego.load.image('BlockD', 'Sprites/Blocks/Block_D.gif');
 juego.load.image('Bg','Sprites/Background/Background.jpg');
 // 'Id','Ruta',Ancho,Alto,Numero de imagenes (-1 la busca automaticamente),espacio entre imagenes, espacio del margen
-juego.load.spritesheet('Spiny','Sprites/Enemy/Spiny_1.png', 16, 16,2,0,0);  
-juego.load.spritesheet('Pj','Sprites/Pj/Mario.png',16,16,7,4,1)
+juego.load.spritesheet('Enemies','Sprites/Enemy/Enemies.png', 32,32,-1,0,0);  
+juego.load.spritesheet('Pj','Sprites/Pj/Mario.png',16,16,3,0,0)
 
 }
 
 var Jugador;
 var Bloque;
-var BlockD;
+var BloqueD;
 var Spiny;
 var Score;
 var MostrarScore;
 var Salto;
 var PosBlock=0; //Defininir la posicion de los bloques
-
+var piso;
+var Pmovil;
 //Controlar los enemigos en un array
 var VSpiny = [];
 
@@ -31,45 +32,60 @@ var VSpiny = [];
 function create(){
   // Cargar las fisicas para un juego arcade
  juego.add.sprite(0, 0, 'Block');
- juego.add.sprite(100, 100, 'BlockD');
+ //juego.add.sprite(100, 100, 'BlockD');
 juego.physics.startSystem(Phaser.Physics.ARCADE);
 juego.world.setBounds(0, 0, 768, 1000); //Establecer limites del juego
 Bloque = juego.add.group();
+BloqueD = juego.add.group();
 Bloque.enableBody = true;
-var piso = Bloque.create(32,32 , 'Block');
-/*for (var i = 0 ; i >24; i++) {
+BloqueD.enableBody= true;
+/*var Pmovil= BloqueD.create(64,64,'BlockD');
+Pmovil.body.immovable=false;*/
+//var piso = Bloque.create(32,32 , 'Block');
+
+for (var i = 0 ; i <24; i++) {
   //Llenar piso de bloques
   PosBlock=32*i;
-  var piso = Bloque.create(PosBlock,32 , 'Block');
+  piso = Bloque.create(PosBlock,968,'Block');
+  piso.body.immovable = true;//No se puede mover
+  //Modificar el sprite
   //juego.add.sprite (PosBlock, 0, 'Block');
-}*/
-//piso.body.immovable = true; //No se puede mover
-Bloque.body.immovable = true;
+}
+ Jugador = juego.add.sprite(32, 836, 'Pj');
+ Jugador.scale.setTo(2,2);
 
-Jugador = juego.add.sprite(32,40, 'Pj');//Añadir jugador
+ //No se puede mover
 
-juego.physics.arcade.enable(Jugador);//Activar fisicas al jugador
+//Bloque.body.immovable = true;
 
-Jugador.body.bounce.y = 0; //El jugador no puede rebotar
-Jugador.body.gravity.y = 500;//Gravedad del jugador
+
+
+juego.physics.arcade.enable(Jugador);//Activar fisicas al Jugador
+
+Jugador.body.bounce.y = 0; //El Jugador no puede rebotar
+Jugador.body.gravity.y = 400;//Gravedad del Jugador
 Jugador.body.collideWorldBounds = true;//Colision con los bordes
 
 //Añadir animaciones
 
 //Jugador.animations.add('left', [0,1,2,3], 10, true); Falta animacion hacia la izquierda D:
-Jugador.animations.add('right', [0, 1,2,3], 10, true);
-Jugador.animations.add('jump', [5], 10, true);
+Jugador.animations.add('right', [0,1], 10, true);
+Jugador.animations.add('left', [0,1], 10, true);
+Jugador.animations.add('jump', [2], 10, true);
 Tecla = juego.input.keyboard.createCursorKeys();
+var LocacionPtos = juego.add.sprite(0, 0);
+LocacionPtos.fixedToCamera = true;
+
 }
 
 
 //Update, un loop equivalente a un step en juegomaker
 function update(){
 
-//  Reseteamos la velocidad del jugador en x, esto nos permitirá evitar que se acelere
-Jugador.body.velocity.x = 0;                
-  // Hacemos colisionar al jugador con las plataformas
-juego.physics.arcade.collide(Jugador, plataformas);
+//  Reseteamos la velocidad del Jugador en x, esto nos permitirá evitar que se acelere
+  Jugador.body.velocity.x = 0;                
+  // Colision
+  juego.physics.arcade.collide(Jugador,Bloque);
 
 if (Tecla.right.isDown)// Si presionamos RIGHT
 {
@@ -78,12 +94,28 @@ Jugador.body.velocity.x = 150;
 Jugador.animations.play('right');
 }
 else{
-  Jugador.animation.stop();
-}
+	if(Tecla.left.isDown){
+		Jugador.body.velocity.x=-150;
+		Jugador.animations.play('left');
 
-}
 
+	}
+	else{
+		Jugador.animations.stop();
+
+	}
+  
+}
+	if(Tecla.up.isDown && Jugador.body.touching.down){
+
+		Jugador.body.velocity.y= -300;
+		Jugador.animations.play('jump');
+
+	}
+}
+/*
 function render() {
 juego.debug.cameraInfo(juego.camera, 32, 32);
-juego.debug.spriteCoords(jugador, 32, 500);
+juego.debug.spriteCoords(Jugador, 32, 500);
 }	
+*/
