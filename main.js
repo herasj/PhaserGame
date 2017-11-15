cbd159b5010cc5afd41e88083de57af29de04dff
herasj <herasj@uninorte.edu.co>
2017-11-15 17:34:56 -0500

    Crear barriles

--- main.js
+++ main.js
@@ -11,11 +11,14 @@ juego.load.image('BlockD', 'Sprites/Blocks/Block_D.gif');
 juego.load.image('Bg','Sprites/Background/Background.jpg');
 // 'Id','Ruta',Ancho,Alto,Numero de imagenes (-1 la busca automaticamente),espacio entre imagenes, espacio del margen
 juego.load.spritesheet('Enemies','Sprites/Enemy/Enemies.png', 32,32,-1,0,0);
-juego.load.spritesheet('Pj','Sprites/Pj/Mario.png',16,16,3,0,0);
-juego.load.spritesheet('Coin','Sprites/Npc/Coin.gif',28,32,4,0,0);
-
+juego.load.spritesheet('Pj','Sprites/Pj/Mario.png',17,16,10,0,0);
+juego.load.spritesheet('Coin','Sprites/Npc/Coin.gif',28,32,4,1,0);
+juego.load.audio('s_coin','Sfx/Coin.wav');
+juego.load.audio('s_jump','Sfx/Jump.wav');
+juego.load.audio('s_cho','Sfx/Bump.wav');
+juego.load.audio('sbg','Sfx/Underworld.mp3')
 }
-
+//MonedaV
 var Jugador;
 var Bloque;
 var BloqueD;
@@ -26,12 +29,17 @@ var Salto;
 var PosBlock=0; //Defininir la posicion de los bloques
 var piso;
 var Pmovil;
+var MonedaO
 var MonedaV = [];
 //Controlar los enemigos en un array
 var VSpiny = [];
 var plataforma;
 var PosPlat;
 var Txt;
+var sndCoin;
+var sndJump;
+var sndchoque;
+var sndBg;
 var Score=0;
 CMonedas=0;
 
@@ -45,35 +53,80 @@ juego.world.setBounds(0, 0, 768, 1000); //Establecer limites del juego
 Bloque = juego.add.group();
 BloqueD = juego.add.group();
 Moneda = juego.add.group();
-
+sndCoin= juego.add.audio('s_coin');
+sndJump = juego.add.audio('s_jump');
+sndchoque= juego.add.audio('s_cho');
+sndBg=juego.add.audio('sbg');
 Bloque.enableBody = true;
 BloqueD.enableBody= true;
+Moneda.enableBody=true;
+
 /*var Pmovil= BloqueD.create(64,64,'BlockD');
 Pmovil.body.immovable=false;*/
 //var piso = Bloque.create(32,32 , 'Block');
 
 //Crear Plataforma
-for (var i = 0; i <11; i++) {
-  PosPlat=((juego.world.height-82)-(80*i))
-  for (var j = 0; j <6 ; j++) {
+
+for (var i = 0; i <7 ; i++) {
+    PosPlat=((juego.world.height-150)-(120*i))
+  for (var j = 0; j< 16 ; j++) {
+
+    plataforma = Bloque.create(265+(16*j),PosPlat,'Block');
+    plataforma.scale.setTo(0.5,0.5);
+
+    plataforma.body.immovable=true;
+
+    MonedaV = Moneda.create(plataforma.x,PosPlat-16,'Coin'); // Crea moneda
+    MonedaV.animations.add('casual',[0,1,2,3],10,true);
+    MonedaV.animations.play('casual');
+    MonedaV.scale.setTo(0.5,0.5);
+  }
+}
+for (var i = 0; i <7; i++) {
+  PosPlat=((juego.world.height-120)-(120*i));
+  for (var j = 0; j<11 ; j++) {
 
       plataforma = Bloque.create(16*j,PosPlat,'Block');
       plataforma.scale.setTo(0.5,0.5);
       plataforma.body.immovable=true;
+
+      MonedaV = Moneda.create(plataforma.x,PosPlat-16,'Coin'); // Crea moneda
+      MonedaV.animations.add('casual',[0,1,2,3],10,true);
+      MonedaV.animations.play('casual');
+      MonedaV.scale.setTo(0.5,0.5);
 }
+}
+
+for (var i = 0; i <7; i++) {
+  PosPlat=((juego.world.height-120)-(120*i));
+  for (var j = 0; j<11 ; j++) {
+
+      plataforma = Bloque.create(600+(16*j),PosPlat,'Block');
+      plataforma.scale.setTo(0.5,0.5);
+      plataforma.body.immovable=true;
+
+      MonedaV = Moneda.create(plataforma.x,PosPlat-16,'Coin'); // Crea moneda
+      MonedaV.animations.add('casual',[0,1,2,3],10,true);
+      MonedaV.animations.play('casual');
+      MonedaV.scale.setTo(0.5,0.5);
+}
+
 }
 for (var i = 0 ; i <48; i++) {
   //Llenar piso de bloques
   PosBlock=16*i;
-  MonedaV[i] = Moneda.create(PosBlock+32,960,'Coin')
+  MonedaV = Moneda.create(PosBlock+32,960,'Coin')
   piso = Bloque.create(PosBlock,984,'Block');
   piso.body.immovable = true;//No se puede mover
-  piso.scale.setTo(0.7,0.7)
+  piso.scale.setTo(0.7,0.7);
+  MonedaV.animations.add('casual',[0,1,2,3],10,true);
+  MonedaV.animations.play('casual');
+  MonedaV.scale.setTo(0.5,0.5);
   //Modificar el sprite
   //juego.add.sprite (PosBlock, 0, 'Block');
 }
- Jugador = juego.add.sprite(32, 885, 'Pj');
- Jugador.scale.setTo(1.42,1.42); //No Cambia
+ Jugador = juego.add.sprite(0,952, 'Pj');
+ Jugador.scale.setTo(1.42,1.42); //Necesita Ajuste *
 
  //No se puede mover
 
@@ -84,28 +137,24 @@ for (var i = 0 ; i <48; i++) {
 juego.physics.arcade.enable(Jugador);//Activar fisicas al Jugador
 
 Jugador.body.bounce.y = 0; //El Jugador no puede rebotar
-Jugador.body.gravity.y = 400;//Gravedad del Jugador
+Jugador.body.gravity.y = 900;//Gravedad del Jugador
 Jugador.body.collideWorldBounds = true;//Colision con los bordes
 
 //Añadir animaciones
-for (var i = 0; i < MonedaV.length; i++) {
-  MonedaV[i].animations.add('casual',[0,1,2,3],10,true);
-  MonedaV[i].animations.play('casual');
-  MonedaV[i].scale.setTo(0.5,0.5);
-}
+
 
 //Jugador.animations.add('left', [0,1,2,3], 10, true); Falta animacion hacia la izquierda D:
-Jugador.animations.add('right', [0,1], 10, true);
-Jugador.animations.add('left', [0,1], 10, true);
-Jugador.animations.add('jump', [2], 10, true);
+Jugador.animations.add('right', [0,6,7,8], 10, true);
+Jugador.animations.add('left', [4,3,2,1], 10, true);
+Jugador.animations.add('jumpl', [5], 10, true);
+Jugador.animations.add('jumpr', [9], 10, true);
+Jugador.animations.add('mario'[0],10,true);
 Tecla = juego.input.keyboard.createCursorKeys();
 var LocacionPtos = juego.add.sprite(0, 0);
 LocacionPtos.fixedToCamera = true;
 juego.add.text(juego.world.width-100, 16, 'Score', {fontSize: '27px', fill: 'white'});
  Txt = juego.add.text(juego.world.width-80, 40, '0', {fontSize: '27px', fill: 'white'});
- LocacionPtos.addChild(Txt);
- LocacionPtos.cameraOffset.x = 10;
- LocacionPtos.cameraOffset.y = 10;
+sndBg.play();
 }
 
 
@@ -117,24 +166,24 @@ function update(){
   // Colision
   juego.physics.arcade.collide(Jugador,Bloque);
 //Colision con Moneda
-for (var i = 0; i <MonedaV.length; i++) {
-  if (Compara_Colision(Jugador,MonedaV[i])==true) {
-    MonedaV[i].kill();
-    Score=Score++;
-    Txt.text=Score;
-  }
-}
+
 
 if (Tecla.right.isDown)// Si presionamos RIGHT
 {
  //  Mover a la derecha
-Jugador.body.velocity.x = 80;
-Jugador.animations.play('right');
+Jugador.body.velocity.x = 120;
+if ( !Tecla.up.isDown) {
+  Jugador.animations.play('right');
+}
+
 }
 else{
 	if(Tecla.left.isDown){
-		Jugador.body.velocity.x=-80;
-		Jugador.animations.play('left');
+		Jugador.body.velocity.x=-120;
+    if ( !Tecla.up.isDown) {
+      Jugador.animations.play('left');
+    }
+
 
 
 	}
@@ -144,17 +193,40 @@ else{
 	}
 
 }
-	if(Tecla.up.isDown && Jugador.body.touching.down){
+	if(Tecla.up.isDown && Jugador.body.touching.down ){
 
-		Jugador.body.velocity.y= -230;
-		Jugador.animations.play('jump');
+		Jugador.body.velocity.y= -450;
+		Jugador.animations.stop();
+    sndJump.play();
 
 	}
+  juego.physics.arcade.overlap(Jugador,Moneda, AddPuntos, null, this);
+  juego.physics.arcade.overlap(Jugador,Bloque,TestChoque,null,this);
+if (Tecla.R.isDown) {
+  game.state.start('main');
+  console.log("Restart");
+}
 }
 
-function Compara_Colision (Jugador,Objeto){
+function TestChoque (Jugador,Bloque){
+if (Jugador.position.y  < Bloque.position.y+32) {
+console.log("Colisiona");
+sndchoque.play();
+}
+}
+
+function AddPuntos (Jugador,Moneda){
+  Moneda.kill();
+  Score=Score+10;
+  Txt.text=Score;
+  sndCoin.play();
+  console.log(Score);
+
+}
+
+/*function Compara_Colision (Jugador,MonedaV){
   var Colisiona;
-  if ((Jugador.position.y + Jugador.height) > Objeto.position.y) {
+  if ((Jugador.position.y + Jugador.height) > MonedaV.position.y) {
       Colisiona = true;
   }
   else{
@@ -163,7 +235,7 @@ function Compara_Colision (Jugador,Objeto){
   return Colisiona;
 }
 
-/*
+
 function render() {
 juego.debug.cameraInfo(juego.camera, 32, 32);
 juego.debug.spriteCoords(Jugador, 32, 500);
